a46c83491cae82d9709a4bf4269ae448cac06fb5
Soham Chakraborty <soham2004c@gmail.com>
2025-11-12 04:55:32 +0530

    modify testcase

--- benchmarks/testcase.c
+++ benchmarks/testcase.c
@@ -1,7 +1,7 @@
 void main(){
     int i, j, x=0, y=0;
-    for(i=0;i<1000;i++){
-        for(j=0;j<10;j++){
+    for(i=0;i<5000;i++){
+        for(j=0;j<2;j++){
             if((i+j)&1) x++;
             else y++;
         }
